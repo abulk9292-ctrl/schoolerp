724431b2a6c6363d0376e79d73db6ee76afaf3e1
ABUL KASIM <abulk9292@gmail.com>
2026-05-02 17:21:41 +0530

    fix allowed hosts

--- school_erp/settings.py
+++ school_erp/settings.py
@@ -6,7 +6,11 @@ SECRET_KEY = 'django-insecure-test-key'
 
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    'alrahman-erp.onrender.com',
+    'localhost',
+    '127.0.0.1',
+]
 
 INSTALLED_APPS = [
     'corsheaders',
