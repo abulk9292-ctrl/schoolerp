b41a8400e861f13eefc1ca0485acb3d90a46dc06
ABUL KASIM <abulk9292@gmail.com>
2026-05-03 22:06:35 +0530

    fix root website and dashboard route

--- school_erp/urls.py
+++ school_erp/urls.py
@@ -3,16 +3,25 @@ from django.urls import path, include
 from core.views import dashboard
 from django.conf import settings
 from django.conf.urls.static import static
+from django.shortcuts import redirect
+
+
+# 🔥 ROOT → WEBSITE
+def home_redirect(request):
+    return redirect('/site/')
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
 
-    # Dashboard (ERP)
-    path('', dashboard, name='dashboard'),
+    # 🔥 ROOT = WEBSITE
+    path('', home_redirect),
 
     # 🔥 PUBLIC WEBSITE
     path('site/', include('website.urls')),
 
+    # 🔥 ERP DASHBOARD (NEW URL)
+    path('dashboard/', dashboard, name='dashboard'),
 
     # Main Modules
     path('academics/', include('academics.urls')),
